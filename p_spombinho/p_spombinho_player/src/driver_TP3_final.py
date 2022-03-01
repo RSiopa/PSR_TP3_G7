5615aa773716432b6ec72204ce097b49eed748e2
sarxell <sarapombinho@ua.pt>
2022-03-01 12:56:39 +0000

    driver_TP3_final.py created with navigation implemented (very arcaic)

--- p_spombinho/p_spombinho_player/src/driver_TP3_final.py
+++ p_spombinho/p_spombinho_player/src/driver_TP3_final.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.8
 import copy
 import math
+import random
+
 import numpy as np
 from numpy.linalg import inv
 import rospy
@@ -13,9 +15,6 @@ from cv_bridge import CvBridge
 from visualization_msgs.msg import *
 import cv2
 import tf2_geometry_msgs # Do not use geometry_msgs. Use this for PoseStamped (depois perguntar porque)
-
-global display_image_front
-global display_image_back
 
 
 class Driver:
@@ -35,6 +34,12 @@ class Driver:
         self.preyPos = PoseStamped()
         self.attackerPos = PoseStamped()
         self.teammatePos = PoseStamped()
+        self.preyPos_back = PoseStamped()
+        self.attackerPos_back = PoseStamped()
+        self.teammatePos_back = PoseStamped()
+        self.Hunting = False
+        self.Running = False
+        self.Navigating = False
         # ----------------------------
         # colors initialization ----------------
         self.attacker_color_min = (0, 0, 239)
@@ -80,8 +85,10 @@ class Driver:
                                         [-1.0, -0.0027, 0.0009, -0.139]])
 
         # subscribes to the back and front images of the car
-        self.image_subscriber_front = rospy.Subscriber(self.node + '/camera/rgb/image_raw', Image, self.GetImage)
-        self.image_subscriber_back = rospy.Subscriber(self.node + '/camera_back/rgb/image_raw', Image, self.GetImage_back)
+        self.image_subscriber_front = message_filters.Subscriber(self.node + '/camera/rgb/image_raw', Image)
+        self.image_subscriber_back = message_filters.Subscriber(self.node + '/camera_back/rgb/image_raw', Image)
+        ts = message_filters.TimeSynchronizer([self.image_subscriber_front, self.image_subscriber_back], 1)
+        ts.registerCallback(self.GetImage)
 
 
     def whichTeam(self):
@@ -129,6 +136,26 @@ class Driver:
         # input : goal
         # output : angle and speed
 
+        if self.Hunting is True:
+            self.goal = self.preyPos  # storing the goal inside the class
+            self.goal.header.frame_id = self.name + '/base_link'
+            print(self.goal)
+            self.goal_active = True
+            print('attack')
+
+        elif self.Running is True:
+            self.goal = self.attackerPos_back  # storing the goal inside the class
+            self.goal.pose.position.x = - self.attackerPos_back.pose.position.x
+            self.goal.pose.position.y = - self.attackerPos_back.pose.position.y
+            self.goal.header.frame_id = self.name + '/base_link'
+            self.goal_active = True
+            print('running')
+
+        else:
+            self.goal_active = False
+            self.Navigating = True
+            print('navigating while waiting goal')
+
         # verify if the goal is achieved
         if self.goal_active:
             distance_to_goal = self.computeDistanceToGoal(self.goal)
@@ -136,6 +163,7 @@ class Driver:
             if distance_to_goal < 0.05:
                 rospy.logwarn('I have achieved my goal!!!')
                 self.goal_active = False
+
 
         # define driving behaviour according to the goal
         if self.goal_active:
@@ -151,7 +179,8 @@ class Driver:
         command_msg = Twist()
         command_msg.linear.x = speed
         command_msg.angular.z = angle
-        self.publisher_goal.publish(command_msg)
+        if self.Navigating is False:
+            self.publisher_goal.publish(command_msg)
 
     def computeDistanceToGoal(self, goal):
 
@@ -194,7 +223,6 @@ class Driver:
         y = goal_in_base_link.pose.position.y
 
         angle = math.atan2(y, x) # compute the angle
-        print(x, y, angle)
 
         distance = math.sqrt(x**2 + y**2)
         speed = 0.5 * (1/distance)
@@ -203,38 +231,27 @@ class Driver:
         speed = max(speed, minimum_speed)
         return angle, speed
 
-    def GetImage(self, data_front):
-        global display_image_front
+    def GetImage(self, data_front, data_back):
         # rospy.loginfo('Image received...')
         image_front = self.br.imgmsg_to_cv2(data_front, "bgr8")
+        image_back = self.br.imgmsg_to_cv2(data_back, "bgr8")
 
         # Convert the image to a Numpy array since most cv2 functions
 
         # require Numpy arrays.
         frame_front = np.array(image_front, dtype=np.uint8)
-
-        # Process the frame using the process_image() function
-        display_image_front = self.discover_car(frame_front, self.lidar2cam)
-        # with this we can see the red, blue and green cars
-        if self.image_flag is True:
-            cv2.imshow('front', display_image_front)
-        cv2.waitKey(1)
-
-    def GetImage_back(self, data_back):
-        global display_image_back
-        # rospy.loginfo('Image received...')
-        image_back = self.br.imgmsg_to_cv2(data_back, "bgr8")
-
-        # require Numpy arrays.
         frame_back = np.array(image_back, dtype=np.uint8)
 
         # Process the frame using the process_image() function
-        display_image_back = self.discover_car(frame_back, self.lidar2cam_back)
+        display_image_front = self.discover_car(frame_front, self.lidar2cam)
+        display_image_back = self.discover_car_back(frame_back, self.lidar2cam_back)
         # with this we can see the red, blue and green cars
         if self.image_flag is True:
+            cv2.imshow('front', display_image_front)
             cv2.imshow('back', display_image_back)
         cv2.waitKey(1)
 
+    # camera que cria o modo de caça
     def discover_car(self, frame, camera_matrix):
         # Convert to HSV
         frame = copy.deepcopy(frame)
@@ -249,9 +266,7 @@ class Driver:
         Center_a, mask_attacker = self.GetCentroid(mask_attacker, frame)
 
         # creates the image
-        # TODO: FILTER SMALL OBJECTS (Separated)
         mask_final = mask_attacker + mask_prey + mask_teammate
-        cv2.imshow('final_mask', mask_final)
         image = cv2.bitwise_or(frame, frame, mask=mask_final)
         image = cv2.add(gray, image)
 
@@ -263,21 +278,60 @@ class Driver:
         self.teammatePos = self.ClosestPoint(Center_t, pixel_cloud)
 
         if math.isinf(self.preyPos.pose.position.x) is False:
-            self.sendMarker(self.preyPos, self.prey_color_max)
+            self.sendMarker(self.preyPos, self.prey_color_max, "prey")
         if math.isinf(self.attackerPos.pose.position.x) is False:
-            self.sendMarker(self.attackerPos, self.attacker_color_max)
+            self.sendMarker(self.attackerPos, self.attacker_color_max, "attacker")
         if math.isinf(self.teammatePos.pose.position.x) is False:
-            self.sendMarker(self.teammatePos, self.teammate_color_max)
+            self.sendMarker(self.teammatePos, self.teammate_color_max, "teammate")
 
         # probably this is for the other function ( we gonna need two flags, one back and other front, so they can't be here
         # comment to stop the car
         if math.isinf(self.preyPos.pose.position.x) is False:
-            self.goal = self.preyPos  # storing the goal inside the class
-            self.goal_active = True
-            # print('attack')
+            self.Hunting = True
         else:
-            self.goal_active = False
-            # print('waiting goal')
+            self.Hunting = False
+        return image
+
+    # Camera que cria o modo de fuga !
+    def discover_car_back(self, frame, camera_matrix):
+        # Convert to HSV
+        frame = copy.deepcopy(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # create 3 channels !
+        gray = cv2.merge((gray, gray, gray))
+        mask_attacker = cv2.inRange(frame, self.attacker_color_min, self.attacker_color_max)
+        mask_prey = cv2.inRange(frame, self.prey_color_min, self.prey_color_max)
+        mask_teammate = cv2.inRange(frame, self.teammate_color_min, self.teammate_color_max)
+        Center_t, mask_teammate = self.GetCentroid(mask_teammate, frame)
+        Center_p, mask_prey = self.GetCentroid(mask_prey, frame)
+        Center_a, mask_attacker = self.GetCentroid(mask_attacker, frame)
+
+        # creates the image
+        mask_final = mask_attacker + mask_prey + mask_teammate
+        image = cv2.bitwise_or(frame, frame, mask=mask_final)
+        image = cv2.add(gray, image)
+
+        # get the transform the lidar values to pixel, draws it in the image
+        pixel_cloud = self.lidar_to_image(camera_matrix, image)
+        # probably here it receives the self.attackerPos , self.preyPos and self.teammatePos in case they exist
+        # it is based by the closest point of the camera centroid
+        self.preyPos_back = self.ClosestPoint(Center_p,  pixel_cloud)
+        self.attackerPos_back = self.ClosestPoint(Center_a, pixel_cloud)
+        self.teammatePos_back = self.ClosestPoint(Center_t, pixel_cloud)
+
+        if math.isinf(self.preyPos_back.pose.position.x) is False:
+            self.sendMarker(self.preyPos_back, self.prey_color_max, "prey")
+        if math.isinf(self.attackerPos_back.pose.position.x) is False:
+            self.sendMarker(self.attackerPos_back, self.attacker_color_max, "attacker")
+        if math.isinf(self.teammatePos_back.pose.position.x) is False:
+            self.sendMarker(self.teammatePos_back, self.teammate_color_max, "teammate")
+
+        # probably this is for the other function ( we gonna need two flags, one back and other front, so they can't be here
+        # comment to stop the car
+        if math.isinf(self.attackerPos_back.pose.position.x) is False:
+            self.Running = True
+        else:
+            self.Running = False
 
         return image
 
@@ -296,21 +350,22 @@ class Driver:
             return Close_lidar_point
         else:
             for idx, pixel in enumerate(pixel_cloud):
-                dist.append(math.sqrt((Center[0]-pixel[0])**2 + (Center[1]-pixel[1])**2))
-                if dist[idx] < 250:
+                # como os valores de trás estao a ir para a frente e vice versa, o melhor é so ver pelo x
+                # dist.append(math.sqrt((Center[0]-pixel[0])**2 + (Center[1]-pixel[1])**2))
+                dist.append(math.sqrt((Center[0] - pixel[0]) ** 2))
+                if dist[idx] < 50:
                     if dist[idx] < flag:
                         Flag_lidar_points.pose.position.x = self.points[idx][0]
                         Flag_lidar_points.pose.position.y = self.points[idx][1]
                         flag = dist[idx]
 
             # point to the base of the robot
-            Close_lidar_point.header.frame_id = self.name + '/base_link'
             # manter-se em inf caso não exista valores proximos
             Close_lidar_point.pose.position.x = Flag_lidar_points.pose.position.x
             Close_lidar_point.pose.position.y = Flag_lidar_points.pose.position.y
             return Close_lidar_point
 
-    def sendMarker(self, coord, color):
+    def sendMarker(self, coord, color, text):
         marker = Marker()
         marker.id = self.id
         marker.header.frame_id = "red1/base_link"
@@ -327,10 +382,32 @@ class Driver:
         marker.pose.position.x = coord.pose.position.x
         marker.pose.position.y = coord.pose.position.y
         marker.pose.position.z = 0.2
+
+        marker2 = Marker()
+        marker2.header.frame_id = "red1/base_link"
+        marker2.type = marker2.TEXT_VIEW_FACING
+        marker2.action = marker2.ADD
+        marker2.scale.x = 0.1
+        marker2.scale.y = 0.1
+        marker2.scale.z = 0.1
+
+        marker2.color.a = 1.0
+        marker2.color.r = 1.0
+        marker2.color.g = 1.0
+        marker2.color.b = 1.0
+        marker2.pose.orientation.w = 0.0
+        marker2.pose.position.x = coord.pose.position.x
+        marker2.pose.position.y = coord.pose.position.y
+        marker2.pose.position.z = 0.6
+        marker2.id = self.id + 1
+        marker2.text = text + ' ' + str(self.id)
+
         self.publish_marker.publish(marker)
-        self.id = self.id + 1
-        if self.id > 5:
+        self.publish_marker.publish(marker2)
+        self.id = self.id + 2
+        if self.id > 6:
             marker.DELETEALL
+            marker2.DELETEALL
             self.id = 0
 
     def lidar_to_image(self, camera_matrix, image):
@@ -339,7 +416,6 @@ class Driver:
         :return:
         """
         # so testar os valores front por agora
-        # TODO: depois mudar para um so for quando estiver funcional
         pixel_cloud =[]
         pixels_final = []
         for value in self.points:
@@ -368,7 +444,8 @@ class Driver:
         :return:
         """
         # creates a list of world coordinates
-        self.points =[]
+        self.points = []
+        move = Twist()
         z = 0
         for idx, range in enumerate(msg.ranges):
             theta = msg.angle_min + idx * msg.angle_increment
@@ -376,14 +453,24 @@ class Driver:
             y = range * math.sin(theta)
             self.points.append([x, y, z, 1])
 
-    def GetCentroid(self, mask, image):
-        cnts = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        for c in cnts:
-            area = cv2.contourArea(c)
-            if area < 100:
-                cv2.drawContours(mask, [c], -1, (0, 0, 0), -1)
+        # we need to detect the obstacles ( meaning that the points we see arent from the prey nor the attacker, so
+        # they are considered obstacles
+        thr1 = 0.8  # Laser scan range threshold
+        thr2 = 0.8
+        if self.Navigating is True:
+            if msg.ranges[0] > thr1 and msg.ranges[15] > thr2 and msg.ranges[345] > thr2:
+                move.linear.x = random.uniform(0.5, 1) # go forward (linear velocity)
+                move.angular.z = 0.0  # do not rotate (angular velocity)
+            else:
+                move.linear.x = 0.0  # stop
+                move.angular.z = random.choice([-0.5, 0.5])  # rotate counter-clockwise
+                if msg.ranges[0] > thr1 and msg.ranges[15] > thr2 and msg.ranges[345] > thr2:
+                    move.linear.x = random.uniform(0.5, 1)
+                    move.angular.z = 0.0
+            self.publisher_goal.publish(move)  # publish the move object
 
+
+    def GetCentroid(self, mask, image):
         # Morph close and invert image
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
